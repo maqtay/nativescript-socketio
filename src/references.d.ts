0808be77d1679eb2c43f9b9be98e3711dcd2447d
Jogboms <jeremiahogbomo@gmail.com>
2017-12-05 12:19:00 +0100

    fix: typo in references

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,5 +1,5 @@
 /// <reference path="./node_modules/tns-core-modules/tns-core-modules.d.ts" />
-/// <reference path="./obj!SocketIO.d.ts" />
+/// <reference path="./objc!SocketIO.d.ts" />
 
 declare const io: any, org: any;
 
