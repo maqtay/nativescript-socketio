3ea87e5e365573f15837303f2aa983890acfe7ac
Jogboms <jeremiahogbomo@gmail.com>
2017-12-05 12:19:27 +0100

    chore: added tns-platform-declarations to references

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,11 +1,8 @@
+/// <reference path="node_modules/tns-platform-declarations/android.d.ts" />
+/// <reference path="node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-core-modules/tns-core-modules.d.ts" />
 /// <reference path="./objc!SocketIO.d.ts" />
 
 declare const io: any, org: any;
 
 declare const SocketAckEmitter;
-
-declare const NSURL;
-declare const NSDictionary;
-declare const NSMutableArray;
-declare const UInt64;
