3876c6b3ade5689e1bb08180ba87ffabf9284173
Jogboms <jeremiahogbomo@gmail.com>
2017-12-09 09:32:13 +0100

    chore(android): added native typings

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,8 +1,7 @@
 /// <reference path="node_modules/tns-platform-declarations/android.d.ts" />
 /// <reference path="node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-core-modules/tns-core-modules.d.ts" />
-/// <reference path="./objc!SocketIO.d.ts" />
+/// <reference path="./typings/objc!SocketIO.d.ts" />
+/// <reference path="./typings/java!SocketIO.d.ts" />
 
-declare const io: any, org: any;
-
-declare const SocketAckEmitter;
+declare const com: any, org: any;
